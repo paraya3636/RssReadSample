0872d2b1a2fd67708361678cb51782868b3fb208
Keisuke Miura <paraya3636@users.noreply.github.com>
2021-04-24 19:44:17 +0900

    記事クラスを作成

---
+++ app/src/main/java/com/example/rssreadsample/domain/model/Article.kt
@@ -0,0 +1,12 @@
+package com.example.rssreadsample.domain.model
+
+import java.util.*
+
+data class Article(
+    val Title: String,
+    val Description: String,
+    val ImageUrl: String,
+    val Date: Date,
+    val WebSiteUrl: String
+) {
+}
