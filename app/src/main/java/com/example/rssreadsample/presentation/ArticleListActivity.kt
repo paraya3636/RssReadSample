aa811fbbe16109507630faf7e399429d48d48eb6
Keisuke Miura <paraya3636@users.noreply.github.com>
2021-04-25 01:49:13 +0900

    Activityのパッケージ移動

--- app/src/main/java/com/example/rssreadsample/presentation/ArticleListActivity.kt
+++ app/src/main/java/com/example/rssreadsample/presentation/ArticleListActivity.kt
@@ -1,9 +1,11 @@
-package com.example.rssreadsample
+package com.example.rssreadsample.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.rssreadsample.R
 
 class ArticleListActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article_list)
